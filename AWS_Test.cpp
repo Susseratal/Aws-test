328693b577e5fbe6d43928cbb74ac6ddb8ce6235
Susseratal <jtfjyrx@gmail.com>
2022-01-14 13:37:48 +0000

    changed credentials to make aws calm down

--- AWS_Test.cpp
+++ AWS_Test.cpp
@@ -133,8 +133,8 @@ int getItem()
         clientConfig.region = "eu-west-2";
         Aws::DynamoDB::Model::GetItemRequest req;
         Aws::Auth::AWSCredentials credentials;
-		credentials.SetAWSAccessKeyId(Aws::String("AKIAR6OZUXZU7HR62RQ7")); // player credentials
-		credentials.SetAWSSecretKey(Aws::String("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"));
+		credentials.SetAWSAccessKeyId(Aws::String("username")); // player credentials
+		credentials.SetAWSSecretKey(Aws::String("password"));
         Aws::DynamoDB::DynamoDBClient dynamoClient(credentials, clientConfig);
 
 
